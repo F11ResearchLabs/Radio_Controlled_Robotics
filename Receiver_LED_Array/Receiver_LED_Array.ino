e86c443f904a44c793e2713c54081739084393eb
F11ResearchLabs <github@f11labs.com>
2023-12-07 20:16:33 -0800

    Update Receiver_LED_Array.ino

--- Receiver_LED_Array/Receiver_LED_Array.ino
+++ Receiver_LED_Array/Receiver_LED_Array.ino
@@ -8,6 +8,11 @@ int led1Pin = 15;
 int led2Pin = 18;
 int led3Pin = 14;
 
+
+const int main_wait = 2000;
+const int flicker_rate = 100;
+
+
 void setup() {
   pinMode(receiver_pin, INPUT);
   pinMode (led1Pin, OUTPUT);
@@ -21,8 +26,51 @@ void setup() {
 void loop(){
 
 
-digitalWrite (led1Pin, HIGH);
+int pulseDuration = pulseIn(receiver_pin, HIGH);
 
+if (pulseDuration < 1000) 
+{
+  // Turn off all LEDs
+  digitalWrite(led1Pin, LOW);
+  digitalWrite(led2Pin, LOW);
+  digitalWrite(led3Pin, LOW);
+} 
+
+else if (pulseDuration >= 1050 && pulseDuration < 1100) 
+{
+  // LED 1 is turned on
+  digitalWrite(led1Pin, HIGH);
+  digitalWrite(led2Pin, LOW);
+  digitalWrite(led3Pin, LOW);
+}
+
+else if (pulseDuration >= 1150 && pulseDuration < 1200) 
+{
+  // LED 1 is turned on
+  digitalWrite(led1Pin, HIGH);
+  digitalWrite(led2Pin, HIGH);
+  digitalWrite(led3Pin, LOW);
+}
+
+
+else if (pulseDuration >= 1250 && pulseDuration < 1300) 
+{
+  // LED 1 is turned on
+  digitalWrite(led1Pin, HIGH);
+  digitalWrite(led2Pin, HIGH);
+  digitalWrite(led3Pin, HIGH);
+}
+
+
+else if (pulseDuration >= 1350 && pulseDuration < 1600) 
+{
+flashy();
+}
+
+else if (pulseDuration >= 1650 && pulseDuration < 1800) 
+{
+flashy2();
+}
 
 
 }
@@ -31,12 +79,40 @@ digitalWrite (led1Pin, HIGH);
 
 
 
+void flashy(){
+
+
+digitalWrite (led1Pin, HIGH);
+digitalWrite (led2Pin, LOW);
+digitalWrite (led3Pin, HIGH);
+delay (flicker_rate);
+digitalWrite (led1Pin, LOW);
+digitalWrite (led2Pin, LOW);
+digitalWrite (led3Pin, LOW);
+delay (flicker_rate);
+
+
+
+}
 
 
 
 
+void flashy2(){
 
 
+digitalWrite (led1Pin, LOW);
+digitalWrite (led2Pin, HIGH);
+digitalWrite (led3Pin, LOW);
+delay (flicker_rate);
+digitalWrite (led1Pin, LOW);
+digitalWrite (led2Pin, LOW);
+digitalWrite (led3Pin, LOW);
+delay (flicker_rate);
+
+
+
+}
 
 
 
